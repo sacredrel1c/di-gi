e41e7294dfad7b70a70369ec36e62f63c2abe6ed
sacredrel1c <tpek07@gmail.com>
2021-07-07 22:19:01 +0300

    live sliders

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,40 @@
 $(function(){
+    var owl = $('#carousel-1');
 
+    owl.owlCarousel({
+        items:1,
+        autoHeight:true
+    });
+
+    $('.right-arrow-carousel').click(function() {
+        owl.trigger('next.owl.carousel',[500]);
+    })
+    $('.left-arrow-carousel').click(function() {
+        owl.trigger('prev.owl.carousel', [500]);
+    })
+    $('.carousel-1').click(function() {
+        owl.trigger('to.owl.carousel', [0, 500]);
+    })
+    $('.carousel-2').click(function() {
+        owl.trigger('to.owl.carousel', [1, 500]);
+    })
+    $('.carousel-3').click(function() {
+        owl.trigger('to.owl.carousel', [2, 500]);
+    })
+
+
+    var owl2 = $('#carousel-2');
+    owl2.owlCarousel({
+        items:1,
+        margin: 10
+    });
+    $('.cite1').click(function() {
+        owl2.trigger('to.owl.carousel', [0, 500]);
+    })
+    $('.cite2').click(function() {
+        owl2.trigger('to.owl.carousel', [1, 500]);
+    })
+    $('.cite3').click(function() {
+        owl2.trigger('to.owl.carousel', [2, 500]);
+    })
 });
